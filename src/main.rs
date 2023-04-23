c941d04f8a14fa99f2619ad7c5340f16a0227920
ibent95 <ibent95tuny@gmail.com>
2023-04-23 08:19:29 +0800

    Learn to print text, get input data, generate number, match the number and add more functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 fn main() {
+    print_hello_world();
+    guest_number();
+}
+
+fn print_hello_world() {
     println!("Hello, world!");
+}
+
+fn guest_number() {
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+    
+    let s_n: i32 = generate_secret_number();
+    
+    compare_guest_to_secret_number(s_n, guess.parse::<i32>().unwrap());
+}
+
+fn generate_secret_number() -> i32 {    
+	let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
+	let secret_string: String = secret_number.to_string();
+
+    println!("The secret number is: {secret_string}");
+    
+    return secret_number;
+}
+
+fn compare_guest_to_secret_number(secret_number: i32, guess: i32) {
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
